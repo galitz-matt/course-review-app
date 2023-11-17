8e44ef15dbb325ac45855db49b4f1b2ee56cbcf3
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-11-17 14:21:51 -0500

    feat(database): create Configuration class for parsing config json

---
+++ src/main/java/edu/virginia/sde/reviews/Configuration.java
@@ -0,0 +1,33 @@
+package edu.virginia.sde.reviews;
+
+import org.json.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Configuration {
+    private final String configFilename = "config.json";
+    private String databaseFilename;
+
+    public String getDatabaseFilename() {
+        if (databaseFilename == null) {
+            parseJsonConfigFile();
+        }
+        return databaseFilename;
+    }
+
+    private void parseJsonConfigFile() {
+        try (InputStream inputStream = Objects.requireNonNull(Configuration.class.getResourceAsStream(configFilename));
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            var jsonContent = bufferedReader.lines().collect(Collectors.joining("\n"));
+            var jsonConfig = new JSONObject(jsonContent);
+            databaseFilename = jsonConfig.get("database").toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
