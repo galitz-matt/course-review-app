d1680b1978bafcb81d61229753ef9cbcb08d7158
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-11-25 22:21:02 -0500

    refactor(gui): remove setters (YAGNI)

--- src/main/java/edu/virginia/sde/reviews/User.java
+++ src/main/java/edu/virginia/sde/reviews/User.java
@@ -34,15 +34,4 @@ public class User {
         return password;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
