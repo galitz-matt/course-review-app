79c56fa6a57a35407ac85f8ca4709a80781adb66
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-11-26 00:28:39 -0500

    feat(gui): add error message label to login screen

--- src/main/java/edu/virginia/sde/reviews/LoginController.java
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -3,12 +3,15 @@ package edu.virginia.sde.reviews;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 
 public class LoginController {
     @FXML
     private TextField usernameField;
     @FXML
     private PasswordField passwordField;
+    @FXML
+    private Label errorMessageLabel;
 
     @FXML
     private void handleLoginAction() {
