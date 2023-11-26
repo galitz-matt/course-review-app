5ee738a26587e9295b772a324937ffedb5e43133
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-11-26 02:15:18 -0500

    refactor: remove error checking from POJOs

--- src/main/java/edu/virginia/sde/reviews/User.java
+++ src/main/java/edu/virginia/sde/reviews/User.java
@@ -6,20 +6,9 @@ public class User {
     private String password;
 
     public User(int id, String username, String password) {
-        verifyInfo(username, password); // TODO: remove; see line 15
         this.id = id;
         this.username= username;
         this.password = password;
-    }
-
-    //TODO: handle this in the business logic i.e. button handlers
-    private void verifyInfo(String username, String password) {
-        if (username == null || password == null) {
-            throw new IllegalArgumentException("User information cannot be empty");
-        }
-        if (password.length() >= 8) {
-            throw new IllegalArgumentException("Password must be less than 8 characters");
-        }
     }
 
     public int getId() {
