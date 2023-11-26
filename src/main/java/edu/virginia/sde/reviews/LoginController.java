600ddd61276e8465dbc2a4cb3ec445dca431b4c0
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-11-25 22:54:29 -0500

    feat(gui): create LoginController

--- src/main/java/edu/virginia/sde/reviews/LoginController.java
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -1,14 +1,4 @@
 package edu.virginia.sde.reviews;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-
 public class LoginController {
-    @FXML
-    private Label messageLabel;
-
-    public void handleButton() {
-        messageLabel.setText("does this work?");
-    }
 }
