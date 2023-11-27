465617910bd28f76b132c7362a78687f5db3134d
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-11-27 14:30:51 -0500

    refactor: remove unused imports

--- src/main/java/edu/virginia/sde/reviews/MainApplication.java
+++ src/main/java/edu/virginia/sde/reviews/MainApplication.java
@@ -1,15 +1,7 @@
 package edu.virginia.sde.reviews;
 
-import com.sun.tools.javac.Main;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.Parent;
-
-import java.util.Objects;
-
-import static javafx.application.Application.launch;
 
 public class MainApplication extends Application {
     public static void main(String[] args) { launch(args); }
